85c6a3d04485af7c089553171464c894f75598a0
QDev <waqiyuddev@gmail.com>
2026-02-05 10:51:13 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,41 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import Header from "@/components/ui/header"
+import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "React Advanced Final Project",
   description: "Coursera Meta Next.js Course Project",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* GLOBAL TOP NAVIGATION */}
+        <Header />
+
+        {/* PAGE CONTENT */}
+        <main className="pt-[64px]">
+          {children}
+        </main>
       </body>
     </html>
-  );
+  )
 }
